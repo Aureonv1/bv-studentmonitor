c13c977688c83393a428f97459bd055388517df6
RNS <ramika.n.perera@gmail.com>
2026-05-25 00:44:15 +0530

    fix

--- admin/admin_auth.php
+++ admin/admin_auth.php
@@ -1,5 +1,23 @@
 <?php
 
+if (!function_exists('str_contains')) {
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return $needle === '' || strpos($haystack, $needle) !== false;
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        if ($needle === '') {
+            return true;
+        }
+
+        return substr($haystack, -strlen($needle)) === $needle;
+    }
+}
+
 if (!function_exists('admin_base_path')) {
     function admin_base_path(): string
     {
